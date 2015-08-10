5e6816078604767a458d6597f1cfe68e03a1d54c
Your Name <you@example.com>
2015-08-10 09:22:50 +0800

    [LN] #N/A back gocd file.

--- backup_gocd_data.sh
+++ backup_gocd_data.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 #back up gocd server data
 
+backdir=/backup/
+
+filename=$(date +%Y-%m-%d).tar
+go_backfile=$backdir/go.$filename
+godb_backfile=$backdir/godb.$filename
+artifact_backfile=$backdir/goartifact.$filename
+
+cd /etc/
+tar -cvp -f $go_backfile go
+
+cd /var/lib/go-server
+tar -cvp -f $godb_backfile db
+
+cd /var/lib/go-server
+tar -cvp -f $artifact_backfile artifacts
